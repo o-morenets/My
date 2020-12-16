2bf9c2353e99a6e58f3120d1f1822f1f2f06c31a
Oleksii Morenets <oleksii_morenets@refinitiv.com>
2020-12-16 21:50:02 +0200

    to binary string

--- src/stackOverflow/toBinaryString/ToBinaryString.java
+++ src/stackOverflow/toBinaryString/ToBinaryString.java
@@ -4,10 +4,6 @@ package stackOverflow.toBinaryString;
 public class ToBinaryString {
 
     public static void main(String[] args) {
-        byte aByte = 3;
-        String byteAsString = Integer.toBinaryString(aByte & 0xff | 0x100).substring(1);
-        System.out.println(byteAsString);
-
         byte[] data = {127,126,125};
 
         for (byte datum : data) {
